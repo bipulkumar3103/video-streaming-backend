929a2fcd90f850ffd153bcd6dd5d91e219623598
Bipul Kumar <bipulkumar3103@gmail.com>
2026-02-22 20:11:37 +0530

    Comppletedfsfsfsa

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import "./workers/video.worker"; // Worker auto-starts when imported
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 (async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT,"0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 })();
